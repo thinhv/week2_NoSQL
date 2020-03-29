5610ccc693bf403b4cac66959236af601e3fb865
Thinh Vo <ducthinh2410@gmail.com>
2020-03-29 09:05:31 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 'use strict';
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
 
 const catRouter = require('./routes/catRoute')
 const userRouter = require('./routes/userRoute')
+
+app.use(cors())
 
 app.use('/cat', catRouter)
 app.use('/user', userRouter)
