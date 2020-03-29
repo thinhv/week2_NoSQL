70a0f7ae30a5bac25bcd6f870e3f23f19c2a219e
Thinh Vo <ducthinh2410@gmail.com>
2020-03-29 16:53:07 +0300

    Create Cat schema

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict';
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -10,10 +12,14 @@ const catRouter = require('./routes/catRoute')
 const userRouter = require('./routes/userRoute')
 const authRouter = require('./routes/authRoute')
 
+const db = require('./db/db')
+
 app.use(cors())
 
 app.use('/cat', passport.authenticate('jwt', {session: false}), catRouter)
 app.use('/user', passport.authenticate('jwt', {session: false}), userRouter)
 app.use('/auth', authRouter)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+db.on('connected', () => {
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+})
--- models/catModel.js
+++ models/catModel.js
@@ -1,23 +1,32 @@
 'use strict';
-const cats = [
-  {
-    id: '1',
-    name: 'Frank',
-    age: '6',
-    weight: '5',
-    owner: '1',
-    filename: 'http://placekitten.com/400/300',
-  },
-  {
-    id: '2',
-    name: 'James',
-    age: '4',
-    weight: '11',
-    owner: '2',
-    filename: 'http://placekitten.com/400/302',
-  },
-];
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-module.exports = {
-  cats,
-};
+const schema = new Schema({
+  name: { type: String, required: true },
+  age: { type: Date, max: Date.now, required: true },
+  gender: { type: String,  enum: ['male', 'female'], required: true },
+  color: String,
+  weight: Number
+})
+
+// const cats = [
+//   {
+//     id: '1',
+//     name: 'Frank',
+//     age: '6',
+//     weight: '5',
+//     owner: '1',
+//     filename: 'http://placekitten.com/400/300',
+//   },
+//   {
+//     id: '2',
+//     name: 'James',
+//     age: '4',
+//     weight: '11',
+//     owner: '2',
+//     filename: 'http://placekitten.com/400/302',
+//   },
+// ];
+
+module.exports = mongoose.model('Cat', schema)
